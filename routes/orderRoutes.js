4c5976282e8d535e5e90644fab73e4798b926a11
jahidhasan6484 <jahidhasan6484@gmail.com>
2023-06-19 23:54:11 +0600

    product update done with payment

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,17 +1,16 @@
 module.exports = (orderCollection, productsCollection) => {
-    const express = require("express");
-    const router = express.Router();
-    const orderController = require("../controllers/orderController");
+  const express = require("express");
+  const router = express.Router();
+  const orderController = require("../controllers/orderController");
 
-    router.get(
-        "/orders",
-        orderController.getAllOrders(orderCollection, productsCollection)
-    );
-    router.post(
-        "/order",
-        orderController.createOrder(orderCollection, productsCollection)
-    );
+  router.get(
+    "/orders",
+    orderController.getAllOrders(orderCollection, productsCollection)
+  );
+  router.post(
+    "/order",
+    orderController.createOrder(orderCollection, productsCollection)
+  );
 
-
-    return router;
+  return router;
 };
