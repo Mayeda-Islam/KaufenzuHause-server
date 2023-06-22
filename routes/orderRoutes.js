090643a2e41ae83cc749aafcf5c63d22567726f1
Miskatur <Miskaturrahman34826@gmail.com>
2023-06-23 04:52:20 +0600

    done get with status, update order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,13 +3,28 @@ module.exports = (orderCollection, productsCollection) => {
   const router = express.Router();
   const orderController = require("../controllers/orderController");
 
+
+  router.post(
+    "/order",
+    orderController.createOrder(orderCollection, productsCollection)
+  );
+
   router.get(
     "/orders",
     orderController.getAllOrders(orderCollection, productsCollection)
   );
-  router.post(
-    "/order",
-    orderController.createOrder(orderCollection, productsCollection)
+  router.get(
+    "/orders/:id",
+    orderController.getOrdersById(orderCollection)
+  );
+  router.get(
+    "/orders/status/:status",
+    orderController.getOrdersByStatus(orderCollection)
+  );
+
+  router.patch(
+    "/orders/:id",
+    orderController.updateOrderById(orderCollection)
   );
 
   return router;
