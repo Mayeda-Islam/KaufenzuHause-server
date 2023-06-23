b1d0d2a275cf0f5f5e646d6b0291ce7c20de2e81
unknown <mayedakonika@gmail.com>
2023-06-23 20:15:54 +0600

    test

--- controllers/aboutUsController.js
+++ controllers/aboutUsController.js
@@ -1,45 +1,49 @@
 const getAboutUs = (aboutUsCollection) => (req, res) => {
-
-    aboutUsCollection.find().toArray()
-        .then(data => {
-            res.json({
-                status: "success",
-                dataCount: data.length,
-                data: data,
-            });
-        })
-        .catch(error => {
-            res.json({
-                status: "fail",
-                message: "can't get the data",
-            })
-        })
-}
+  aboutUsCollection
+    .find()
+    .toArray()
+    .then((data) => {
+      res.json({
+        status: "success",
+        dataCount: data.length,
+        data: data,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        status: "fail",
+        message: "can't get the data",
+      });
+    });
+};
 
 const createAboutUs = (aboutUsCollection) => (req, res) => {
+  const data = req.body;
+  aboutUsCollection
+    .insertOne(data)
+    .then((data) => {
+      res.json({
+        status: "success",
+        message: "Data inserted Successfully",
 
-    const data = req.body;
-    aboutUsCollection.insertOne(data)
-        .then(data => {
-            res.json({
-                status: 'success',
-                message: 'Data inserted Successfully',
-
-                data: result
-            })
-
-
-
-        })
-        .catch(error => {
-            res.json({
-                status: "fail",
-                message: "can't get the data",
-            })
-        })
-}
+        data: result,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        status: "fail",
+        message: "can't get the data",
+      });
+    });
+};
 
 module.exports = {
-    getAboutUs,
-    createAboutUs
+  getAboutUs,
+  createAboutUs,
 };
+
+{
+  /* <Badge badgeContent={cart?.length} color="primary">
+<CgShoppingCart color="action" />
+</Badge> */
+}
