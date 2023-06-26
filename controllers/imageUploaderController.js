b3de3f015fe0f32e4a4fd4decb10a0e80e9799bb
Miskatur <Miskaturrahman34826@gmail.com>
2023-06-26 23:48:43 +0600

    done

--- controllers/imageUploaderController.js
+++ controllers/imageUploaderController.js
@@ -2,7 +2,7 @@ const singleImage = async (req, res) => {
     try {
         res.json({
             status: "success",
-            url: `http://localhost:5000/${req.file.filename}`,
+            url: `https://server.kaufenzuhause.de/${req.file.filename}`,
         });
     } catch (err) { }
 };
@@ -11,7 +11,7 @@ const multiImageUploads = async (req, res) => {
     try {
         const imageUrl = [];
         req.files.forEach((img) => {
-            imageUrl.push(`http://localhost:5000/${img.filename}`);
+            imageUrl.push(`https://server.kaufenzuhause.de/${img.filename}`);
         });
         res.status(200).json({
             status: "success",
@@ -27,7 +27,7 @@ const bannerSliderImage = async (req, res) => {
         console.log(req.file)
         res.json({
             status: "success",
-            url: `http://localhost:5000/${req.file.filename}`,
+            url: `https://server.kaufenzuhause.de/${req.file.filename}`,
         });
     } catch (err) { }
 }
